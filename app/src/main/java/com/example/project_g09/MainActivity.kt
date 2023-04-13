bafc969ea91f19c9a60c946e5ac2186ad90c029e
Arnoldo Bermudez <53409812+armabecu@users.noreply.github.com>
2023-04-13 15:13:30 -0400

    Bottom Navigation now changes between fragments

--- app/src/main/java/com/example/project_g09/MainActivity.kt
+++ app/src/main/java/com/example/project_g09/MainActivity.kt
@@ -2,13 +2,63 @@ package com.example.project_g09
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // setup the bottom navigation menu so it responds to clicks
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.container1) as NavHostFragment
+        val navController = navHostFragment.navController
+        setupWithNavController(findViewById<BottomNavigationView>(R.id.bottomNavigationView), navController)
+
     }
+
+
+//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//        val inflater: MenuInflater = menuInflater
+//        inflater.inflate(R.menu.options_menu, menu)
+//        return true
+//    }
+
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        // Handle item selection
+//        when (item.itemId) {
+//            R.id.mi_settings -> {
+//                val toast = Toast.makeText(this, "Settings Menu clicked!", Toast.LENGTH_SHORT)
+//                toast.show()
+//                return true
+//            }
+//            R.id.mi_profile -> {
+//                val toast = Toast.makeText(this, "Profile Menu clicked!", Toast.LENGTH_SHORT)
+//                toast.show()
+//                return true
+//            }
+//            R.id.mi_logout -> {
+//                val toast = Toast.makeText(this, "Logout Menu clicked!", Toast.LENGTH_SHORT)
+//                toast.show()
+//                return true
+//            }
+//            else -> {
+//                return super.onOptionsItemSelected(item)
+//            }
+//        }
+//    }
+
 
 
 }
