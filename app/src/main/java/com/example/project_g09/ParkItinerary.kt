50a018e64bfae0c3981eaf39ade303a66c580bf5
TittyMcTwister <aayush.rattan@ymail.com>
2023-04-18 18:25:30 -0400

    Fixed Itinerary Bugs and Added Remove Button

--- app/src/main/java/com/example/project_g09/ParkItinerary.kt
+++ app/src/main/java/com/example/project_g09/ParkItinerary.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.project_g09.databinding.FragmentItineraryBinding
 import com.example.project_g09.databinding.FragmentParkItineraryBinding
@@ -39,45 +41,68 @@ class ParkItinerary : Fragment(R.layout.fragment_park_itinerary) {
     }
 
     private fun doStuff() {
-        val preferences = requireActivity().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+        val preferences = requireActivity().getSharedPreferences(args.fullName, Context.MODE_PRIVATE)
         val name = args.fullName
         val address = args.address
         binding.tvFullName.text = name
         binding.tvAddress.text = address
-        binding.etDate.setText(preferences.getString("date", ""))
-        binding.etNotes.setText(preferences.getString("notes", ""))
+        binding.etDate.setText(preferences.getString("${args.fullName}_date", ""))
+        binding.etNotes.setText(preferences.getString("${args.fullName}_notes", ""))
 
         binding.btnSave.setOnClickListener {
             val date = binding.etDate.text.toString()
             val notes = binding.etNotes.text.toString()
 
             preferences.edit().apply {
-                putString("date", date)
-                putString("notes", notes)
+                putString("${args.fullName}_date", date)
+                putString("${args.fullName}_notes", notes)
                 apply()
+                db.collection("parks")
+                    .whereEqualTo("fullName", args.fullName)
+                    .get()
+                    .addOnSuccessListener { documents ->
+                        for (document in documents) {
+                            val parkDocRef = db.collection("parks").document(document.id)
+                            val date = binding.etDate.text.toString()
+                            parkDocRef.update("currentDate", date)
+                                .addOnSuccessListener {
+                                    // Date field updated successfully
+                                    Toast.makeText(requireContext(), "Itinerary Saved", Toast.LENGTH_SHORT).show()
+                                }
+                        }
+                    }
+                    .addOnFailureListener { exception ->
+                        // Handle the error here
+                    }
             }
         }
+        binding.btnRemove.setOnClickListener {
+            db.collection("parks")
+                .whereEqualTo("fullName", args.fullName)
+                .get()
+                .addOnSuccessListener { documents ->
+                    for (document in documents) {
+                        val parkDocRef = db.collection("parks").document(document.id)
+                        parkDocRef.delete().addOnSuccessListener {
+                            Toast.makeText(
+                                requireContext(),
+                                "Itinerary Removed",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            findNavController().popBackStack()
+                            val date = binding.etDate.text.toString()
+                            val notes = binding.etNotes.text.toString()
 
-        db.collection("parks")
-            .whereEqualTo("fullName", args.fullName)
-            .get()
-            .addOnSuccessListener { documents ->
-                for (document in documents) {
-                    val parkDocRef = db.collection("parks").document(document.id)
-                    val date = binding.etDate.text.toString()
-                    parkDocRef.update("currentDate", date)
-                        .addOnSuccessListener {
-                            // Date field updated successfully
+                            preferences.edit().apply {
+                                remove("${args.fullName}_date")
+                                remove("${args.fullName}_notes")
+                                apply()
+                            }
                         }
-                        .addOnFailureListener { e ->
-                            // Handle the error here
-                        }
+                    }
                 }
-            }
-            .addOnFailureListener { exception ->
-                // Handle the error here
-            }
 
+        }
     }
 
     override fun onDestroyView() {
